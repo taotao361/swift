441514d2a830b907c213ea3a9b6416f6d61e1529
yangxutao <yangxutao361@163.com>
2017-05-09 17:11:58 +0800

    c

--- FirstSwiftDemo/ViewController.swift
+++ FirstSwiftDemo/ViewController.swift
@@ -40,7 +40,169 @@ class ViewController: UIViewController {
         
         
         
+        
+        
     }
+    
+    //实例方法 和 类方法
+    func test5() -> Void {
+        //方法
+        //实例方法
+        //结构体和枚举能够定义方法是 Swift 与 C/Objective-C 的主要区别之一。在 Objective-C 中，类是唯一能定义方法的类型。但在 Swift 中，你不仅能选择是否要定义一个类/结构体/枚举，还能灵活地在你创建的类型（类/结构体/枚举）上定义方法。
+        class Counter {
+            var count = 0
+            func increment() -> Void {
+                count += 1
+            }
+            func increment(by aCount : Int)  {
+                count += aCount
+            }
+            
+            func reset () {
+                count = 0
+            }
+        }
+        
+        /*
+         Counter类定义了三个实例方法：
+         
+         increment让计数器按一递增；
+         increment(by: Int)让计数器按一个指定的整数值递增；
+         reset将计数器重置为0。
+         Counter这个类还声明了一个可变属性count，用它来保持对当前计数器值的追踪。
+         */
+        let counter = Counter()
+        counter.increment()
+        counter.increment(by: 10)
+        counter.reset()
+        counter.increment(by: 20)
+        print(counter.count)
+        //函数参数可以同时有一个局部名称（在函数体内部使用）和一个外部名称（在调用函数时使用），详情参见指定外部参数名。方法参数也一样，因为方法就是函数，只是这个函数与某个类型相关联了。
+        //self属性  类型的每一个实例都有一个隐含属性叫做self，self完全等同于该实例本身。你可以在一个实例的实例方法中使用这个隐含的self属性来引用当前实例。
+        //上面例子中的increment方法还可以这样写：
+        //        func increment() {
+        //            self.count += 1
+        //        }
+        //使用这条规则的主要场景是实例方法的某个参数名称与实例的某个属性名称相同的时候。在这种情况下，参数名称享有优先权，并且在引用属性时必须使用一种更严格的方式。这时你可以使用self属性来区分参数名称和属性名称。
+        struct Point {
+            var x = 0.0,y = 0.0
+            func theRightX(x : Double) -> Bool {
+                return self.x > x
+            }
+        }
+        
+        let somePoint = Point(x : 4.0,y : 5.0)
+        let isBig = somePoint.theRightX(x: 3.0)
+        if isBig {
+            print("big")
+        }
+        
+        //在实例方法中修改值类型
+        //结构体和枚举是值类型，默认情况下，值类型的属性不能在它的实例方法中被修改；
+        //但是，如果你确实需要在某个特定的方法中修改结构体或者枚举的属性，你可以为这个方法选择可变(mutating)行为，然后就可以从其方法内部改变它的属性；并且这个方法做的任何改变都会在方法执行结束时写回到原始结构中。方法还可以给它隐含的self属性赋予一个全新的实例，这个新实例在方法结束时会替换现存实例。
+        
+        // 要使用可变方法，将关键字mutating 放到方法的func关键字之前就可以了：
+        struct MutablePoint {
+            var x = 0.0,y = 0.0
+            mutating func moveByX(deltaX:Double,deltaY:Double) {
+                x += deltaX
+                y += deltaY
+            }
+        }
+        
+        var mutablePoint = MutablePoint(x:2.0,y:3.0)
+        mutablePoint.moveByX(deltaX: 3.0, deltaY: 4.0)
+        print("the point noew is \(mutablePoint.x)   \(mutablePoint.y)")
+        //上面的Point结构体定义了一个可变方法 moveByX(_:y:) 来移动Point实例到给定的位置。该方法被调用时修改了这个点，而不是返回一个新的点。方法定义时加上了mutating关键字，从而允许修改属性。注意，不能在结构体类型的常量（a constant of structure type）上调用可变方法，因为其属性不能被改变，即使属性是变量属性
+        
+        //在可变方法中给属性赋值
+        struct MutablePoint1 {
+            var x = 0.0,y = 0.0
+            mutating func moveBy (x deltaX : Double,y deltaY : Double) {
+                self = MutablePoint1(x : x + deltaX,y : y + deltaY)
+            }
+        }
+        
+        //枚举的可变方法可以把self设置为同一枚举类型中不同的成员：
+        enum Switch {
+            case Off,High,Low
+            mutating func next() {
+                switch self {
+                case .Off:
+                    self = .Low
+                case .Low:
+                    self = .High
+                default:
+                    self = .High
+                }
+            }
+        }
+        
+        
+        //类型方法   类方法
+        //实例方法是被某个类型的实例调用的方法。你也可以定义在类型本身上调用的方法，这种方法就叫做类型方法。在方法的func关键字之前加上关键字class，来指定类型方法。类还可以用关键字class来允许子类重写父类的方法实现。
+        //       类  在方法 func 的前边加上 <#class#> 关键字，来制定类型方法，类还可以使用关键字 class 来允许子类重写父类的方法实现
+        //        在OC中，你只能为类添加类方法，在swift中，你可以为所有的嘞、结构体。、枚举添加类方法，使用关键字<#static#>  每一个类型方法都被他所支持的类型所包含
+        //        使用点语法调用
+        class SomeClass {
+            class func someMethod() {
+                print("这是一个类方法")
+            }
+        }
+        SomeClass.someMethod()
+        
+        
+        
+        struct LevelTracker {
+            static var highUnlockedLevel = 1
+            var currentLevel = 1
+            static func unlock(_ level : Int) {
+                if level > highUnlockedLevel {
+                    highUnlockedLevel = level
+                }
+            }
+            
+            static func isUnlocked(_ level : Int) -> Bool {
+                return level < highUnlockedLevel
+            }
+            
+            @discardableResult
+            mutating func advance(to level : Int) -> Bool {
+                if LevelTracker.isUnlocked(level) {
+                    currentLevel = level
+                    return true
+                } else {
+                    return false
+                }
+            }
+        }
+        
+        
+        class Player {
+            var tracker = LevelTracker()
+            let playerName : String
+            func complete(level : Int) {
+                LevelTracker.unlock(level + 1)
+                tracker.advance(to: level + 1)
+            }
+            init(name : String) {
+                playerName = name
+            }
+        }
+        
+        var  playerOne = Player(name : "xiaohong")
+        playerOne.complete(level: 1)
+        print("highest unlocked level is now \(LevelTracker.highUnlockedLevel)")
+        
+        playerOne = Player(name : "hehe")
+        if playerOne.tracker.advance(to: 6) {
+            print("noe level is 6")
+        } else {
+            print("unLock")
+        }
+
+    }
+    
     
     //属性
     func test4() {
