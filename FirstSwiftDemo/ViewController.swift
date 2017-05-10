b7128539ff375afb5c807c4191dad0713bc815c4
yangxutao <yangxutao361@163.com>
2017-05-10 16:05:44 +0800

    c

--- FirstSwiftDemo/ViewController.swift
+++ FirstSwiftDemo/ViewController.swift
@@ -41,12 +41,62 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        
 
-
         
         
         
         
+    }
+    
+    //析构过程
+    func test9() {
+        //析构过程
+        //析构器只适用于类类型，当一个类的实例被释放之前，析构器会被立即调用。析构器用关键字deinit来标示，类似于构造器要用init来标示。
+        
+        // 析构过程原理
+        //Swift 会自动释放不再需要的实例以释放资源。如自动引用计数章节中所讲述，Swift 通过自动引用计数（ARC）处理实例的内存管理。通常当你的实例被释放时不需要手动地去清理。但是，当使用自己的资源时，你可能需要进行一些额外的清理。例如，如果创建了一个自定义的类来打开一个文件，并写入一些数据，你可能需要在类实例被释放之前手动去关闭该文件。
+        
+        //在类的定义中，每个类最多只能有一个析构器，而且析构器不带任何参数，如下所示：
+        //        deinit {
+        //
+        //        }
+        
+        
+        class Bank {
+            static var coinsInBank = 10000
+            static func distribute(coins numberCoinsRequired : Int) -> Int {
+                let numberOfCoin = min(numberCoinsRequired, coinsInBank)
+                coinsInBank -= numberOfCoin
+                return numberOfCoin
+            }
+            static func receive (coins : Int) {
+                coinsInBank += coins
+            }
+        }
+        
+        class Player {
+            var coinsInPurse : Int
+            init(coins : Int) {
+                coinsInPurse = Bank.distribute(coins: coins)
+            }
+            func win(coins : Int) {
+                coinsInPurse += Bank.distribute(coins: coins)
+            }
+            deinit { //对象时放就会调用 类似于OC中的 dealloc
+                Bank.receive(coins: coinsInPurse)
+            }
+        }
+        
+        var playerOne : Player? = Player.init(coins: 100)
+        print("a new player has joined the game with \(playerOne!.coinsInPurse) coins") //因为playerOne是可选的，所以访问其coinsInPurse属性来打印钱包中的硬币数量时，使用感叹号（!）来解包：
+        playerOne?.win(coins: 1000)
+        playerOne!.win(coins: 1000)
+        print("------- \(playerOne!.coinsInPurse)")
+        
+        
+        playerOne = nil
+        print(" ------ bank.coins = \(Bank.coinsInBank)")
         
     }
     
