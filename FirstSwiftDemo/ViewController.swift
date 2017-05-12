74a5e9703e7245c2a8bc8ae9316a44e32ce60ff2
yangxutao <yangxutao361@163.com>
2017-05-12 09:59:20 +0800

    c

--- FirstSwiftDemo/ViewController.swift
+++ FirstSwiftDemo/ViewController.swift
@@ -246,7 +246,75 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
+        //类型转换
+        //类型转换 可以判断实例的类型，也可以将实例看做是其父类或者子类的实例。
+        //类型转换在 Swift 中使用 is 和 as 操作符实现。这两个操作符提供了一种简单达意的方式去检查值的类型或者转换它的类型。
         
+        class Library {
+            let name : String
+            init(name : String) {
+                self.name = name
+            }
+        }
+        
+        class Movie : Library {
+            var director : String
+            init(name: String,director : String) {
+                self.director = director
+                super.init(name: name)
+            }
+        }
+        
+        class Song : Library {
+            var artist : String
+            init(name : String,artist : String) {
+                self.artist = artist
+                super.init(name: name)
+            }
+        }
+        
+        let group = [
+            Movie.init(name: "a", director: "AA"),
+            Movie.init(name: "b", director: "BB"),
+            Song.init(name: "c", artist: "CC"),
+            Song.init(name: "d", artist: "DD"),
+            ]
+        //类型检查，用 类型检查操作符（is）来检查一个实例是否属于特定子类型。若实例属于那个子类型，类型检查操作符返回 true，否则返回 false。<# is 是检查 子类是不是 ，某类的 特定子类型 类似于isKindOfClass#>
+        var songCount = 0,movieCount = 0
+        for item in group {
+            if item is Movie {
+                movieCount += 1
+            } else if item is Song {
+                songCount += 1
+            }
+        }
+        
+        //向下转型
+        //某类型的一个常量或变量可能在幕后实际上属于一个子类。当确定是这种情况时，你可以尝试向下转到它的子类型，用类型转换操作符（as? 或 as!）。as? as!
+        
+        /*
+         因为向下转型可能会失败，类型转型操作符带有两种不同形式。条件形式as? 返回一个你试图向下转成的类型的可选值。强制形式 as! 把试图向下转型和强制解包（转换结果结合为一个操作。
+         
+         当你不确定向下转型可以成功时，用类型转换的条件形式（as?）。条件形式的类型转换总是返回一个可选值，并且若下转是不可能的，可选值将是 nil。这使你能够检查向下转型是否成功。
+         
+         只有你可以确定向下转型一定会成功时，才使用强制形式（as!）。当你试图向下转型为一个不正确的类型时，强制形式的类型转换会触发一个运行时错误。
+         
+         下面的例子，迭代了 library 里的每一个 MediaItem，并打印出适当的描述。要这样做，item 需要真正作为 Movie 或 Song 的类型来使用，而不仅仅是作为 MediaItem。为了能够在描述中使用 Movie 或 Song 的 director 或 artist 属性，这是必要的。
+         
+         在这个示例中，数组中的每一个 item 可能是 Movie 或 Song。事前你不知道每个 item 的真实类型，所以这里使用条件形式的类型转换（as?）去检查循环里的每次下转：
+         */
+        
+        
+        for item in group {
+            if let movie = item as? Movie {
+                print("movie name = \(movie.name)   director = \(movie.director)")
+            } else if let song = item as? Song {
+                print("song name = \(song.name) artist = \(song.artist)")
+            }
+        }
+        
+        
+
         
         
         
