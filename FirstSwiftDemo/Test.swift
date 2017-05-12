a50ca5d31423bdb3780dd50b6fc579b3ca3dcdce
yangxutao <yangxutao361@163.com>
2017-05-12 18:50:54 +0800

    c

--- FirstSwiftDemo/Test.swift
+++ FirstSwiftDemo/Test.swift
@@ -9,12 +9,57 @@
 import UIKit
 
 
+protocol SomeProtocol {
+    var mustBeSettable: Int { get set }
+    var doesNotNeedToBeSettable: Int { get }
+}
+
+protocol AnotherProtocol {
+    //类属性
+    static var someTypeProperty: Int { get set }
+}
+
+protocol Bprotocol {
+    static var name : String {get set}
+}
+
 extension Test {
-    func a () {
-        
+    class HAHA {
+        let name : String
+        init(name : String) {
+            self.name = name
+        }
     }
+    
+    enum BBB {
+        case A
+        case B
+        case C
+    }
+
+    func goToSwimming()  {
+        print("go to swimming")
+    }
+    
 }
 
 class Test: NSObject {
-
+    
+    var count = 0
+    var money = 10.0
+    var hobby = ["pingpang","basketBall","soccer"]
+    
+   init(money : Double,count : Int) {
+        self.count = count
+        self.money = money
+    }
+    
+    func goToSport() -> Void {
+        print("去运动")
+    }
+    
+    func play(givenMoney : Double) {
+        print("============= \(givenMoney)")
+    }
+    
 }
--- FirstSwiftDemo/ViewController.swift
+++ FirstSwiftDemo/ViewController.swift
@@ -238,16 +238,267 @@ struct BlackjackCard {
 }
 
 
+//协议
+//只含有一个实例属性要求的协议
+//FullyNamed 协议除了要求遵循协议的类型提供 fullName 属性外，并没有其他特别的要求。这个协议表示，任何遵循 FullyNamed 的类型，都必须有一个可读的 String 类型的实例属性 fullName。
+protocol FullyNamed {
+    var fullName: String { get }
+}
+
+
+protocol Togglable {
+    mutating func toggle()
+}
+
+
+
+//构造器协议
+protocol GeneratorProtocol {
+    init(name : String)
+}
+
+protocol PeopleProtocol {
+    init()
+}
+
+//委托模式
+//骰子游戏
+protocol DiceGame {
+    var dice : Dice {get}
+    func play()
+}
+
+protocol DiceGameDelegate {
+    func gameDidStart(_ game : DiceGame)
+    func game(_ game : DiceGame,didStartNewTurnWithRoll diceRoll : Int)
+    func gameDidEnd(_ game : DiceGame)
+}
+
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c).truncatingRemainder(dividingBy:m))
+        return lastRandom / m
+    }
+}
+
+class Dice {
+    let sides : Int
+    let generator : RandomNumberGenerator
+    init(sides : Int,generator : RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    func roll () -> Int {
+        return Int(generator.random() * Double(sides)) + 1 //类型转换 Int()
+    }
+}
+
+class SnakesAndLadders : DiceGame {
+    let finalSquare = 25
+    let dice = Dice.init(sides: 6, generator: LinearCongruentialGenerator())
+    var square = 0
+    var board : [Int]
+    init() {
+        board = [Int].init(repeating: 0, count: finalSquare + 1)
+        board[03] = +08; board[06] = +11; board[09] = +09; board[10] = +02
+        board[14] = -10; board[19] = -11; board[22] = -02; board[24] = -08
+    }
+    
+    var delegate : DiceGameDelegate?
+    
+    func play() {
+        square = 0
+        delegate?.gameDidStart(self)
+        gameLoop : while square != finalSquare {
+            let diceRoll = dice.roll()
+            delegate?.game(self, didStartNewTurnWithRoll: diceRoll)
+            switch square + diceRoll {
+            case finalSquare:
+                break gameLoop
+            case let newSquare where newSquare > finalSquare:
+                continue gameLoop
+            default:
+                square += diceRoll
+                square += board[square]
+            }
+        }
+        delegate?.gameDidEnd(self)
+    }
+}
+
+
+class DiceGameTracker: DiceGameDelegate {
+    var numberOfTurns = 0
+    func gameDidStart(_ game: DiceGame) {
+        numberOfTurns = 0
+        if game is SnakesAndLadders {
+            print("Started a new game of Snakes and Ladders")
+        }
+        print("The game is using a \(game.dice.sides)-sided dice")
+    }
+    func game(_ game: DiceGame, didStartNewTurnWithRoll diceRoll: Int) {
+        numberOfTurns += 1
+        print("Rolled a \(diceRoll)")
+    }
+    func gameDidEnd(_ game: DiceGame) {
+        print("The game lasted for \(numberOfTurns) turns")
+    }
+}
+
 
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
+        //协议
+        //类、结构体或枚举都可以遵循协议
+        //可以对协议进行扩展
+        
+        //协议语法
+        //协议的定义方式与类、结构体和枚举的定义非常相似：
+//        protocol SomeProtocol {
+//            // 这里是协议的定义部分
+//        }
+        
+//        struct SomeStructure: FirstProtocol, AnotherProtocol {
+//            // 这里是结构体的定义部分
+//        }
+        
+//        拥有父类的类在遵循协议时，应该将父类名放在协议名之前，以逗号分隔
+//        class SomeClass: SomeSuperClass, FirstProtocol, AnotherProtocol {
+//            // 这里是类的定义部分
+//        }
+        
+
+        //属性要求
+//        协议不指定属性是存储型属性还是计算型属性，它只指定属性的名称和类型。此外，协议还指定属性是可读的还是可读可写的
+//        协议总是用 var 关键字来声明变量属性，在类型声明后加上 { set get } 来表示属性是可读可写的，可读属性则用 { get } 来表示：
+//        protocol SomeProtocol {
+//            var mustBeSettable: Int { get set }
+//            var doesNotNeedToBeSettable: Int { get }
+//        }
+
+//        在协议中定义类型属性时，总是使用 static 关键字作为前缀。当类类型遵循协议时，除了 static 关键字，还可以使用 class 关键字来声明类型属性：
+//        protocol AnotherProtocol {
+//            static var someTypeProperty: Int { get set }
+//        }
+        
+
+        struct Person : FullyNamed {
+            var fullName: String
+        }
+        
+        //Starship 类把 fullName 属性实现为只读的计算型属性。每一个 Starship 类的实例都有一个名为 name 的非可选属性和一个名为 prefix 的可选属性。 当 prefix 存在时，计算型属性 fullName 会将 prefix 插入到 name 之前，从而为星际飞船构建一个全名。
+        class StarShip : FullyNamed {
+            var prefix : String
+            var name : String
+            init(name : String,prefix : String? = nil) {
+                self.name = name
+                self.prefix = prefix!
+            }
+            var fullName: String {
+                return (prefix != nil ? prefix + "" : "") + name
+            }
+        }
+        
+        //方法要求
+        //协议可以要求遵循协议的类型实现某些指定的实例方法或类方法。这些方法作为协议的一部分，像普通方法一样放在协议的定义中，但是不需要大括号和方法体。可以在协议中定义具有可变参数的方法，和普通方法的定义方式相同。但是，不支持为协议中的方法的参数提供默认值。
+        //正如属性要求中所述，在协议中定义类方法的时候，总是使用 static 关键字作为前缀。当类类型遵循协议时，除了 static 关键字，还可以使用 class 关键字作为前缀：
         
         
+
+        //Mutating 方法要求
+        //有时需要在方法中改变方法所属的实例。例如，在值类型（即结构体和枚举）的实例方法中，将 mutating 关键字作为方法的前缀，写在 func 关键字之前，表示可以在该方法中修改它所属的实例以及实例的任意属性的值。这一过程在在实例方法中修改值类型章节中有详细描述。
+        
+        //如果你在协议中定义了一个实例方法，该方法会改变遵循该协议的类型的实例，那么在定义协议时需要在方法前加 mutating 关键字。这使得结构体和枚举能够遵循此协议并满足此方法要求。
+        
+        //实现协议中的 mutating 方法时，若是类类型，则不用写 mutating 关键字。而对于结构体和枚举，则必须写 mutating 关键字
+        
+        enum OnOffSwitch : Togglable {
+            case on,off
+            mutating func toggle() {
+                switch self {
+                case .on:
+                    self = .off
+                case.off:
+                    self = .on
+                }
+            }
+        }
+        
+        var light = OnOffSwitch.off
+        light.toggle()
+        
+        
+        //构造器要求
+        //协议可以要求遵循协议的类型实现指定的构造器。你可以像编写普通构造器那样，在协议的定义里写下构造器的声明，但不需要写花括号和构造器的实体：
+        //你可以在遵循协议的类中实现构造器，无论是作为指定构造器，还是作为便利构造器。无论哪种情况，你都必须为构造器实现标上 required 修饰符：
+        //使用 required 修饰符可以确保所有子类也必须提供此构造器实现，从而也能符合协议。
+        //如果类已经被标记为 final，那么不需要在协议构造器的实现中使用 required 修饰符，因为 final 类不能有子类。关于 final 修饰符的更多内容，请参见防止重写。在类前边 加上 final 关键字，表明类不能被继承
+        class Zero : GeneratorProtocol {
+            var name : String
+            required init(name: String) {
+                self.name = name
+            }
+        }
+        
+        //如果一个子类重写了父类的指定构造器，并且该构造器满足了某个协议的要求，那么该构造器的实现要同事标注 required 和 override 修饰符
+        
+        class People {
+            init() {
+            }
+        }
+        
+        class Man : People ,PeopleProtocol {
+            // 因为遵循协议，需要加上 required
+            // 因为继承自父类，需要加上 override
+            required override init() {
+                
+            }
+        }
+        
+        
+        //可失败构造器要求
+        //协议还可以为遵循协议的类型定义可失败构造器要求，详见可失败构造器。
+        //遵循协议的类型可以通过可失败构造器（init?）或非可失败构造器（init）来满足协议中定义的可失败构造器要求。协议中定义的非可失败构造器要求可以通过非可失败构造器（init）或隐式解包可失败构造器（init!）来满足。
+        
+        
+        
+        //协议作为类型
+        //尽管协议本身并未实现任何功能，但是协议可以被当做一个成熟的类型类使用
+        /*
+         协议可以像其他普通类型一样使用，使用场景如下：
+        作为函数、方法或构造器中的参数类型或返回值类型
+        作为常量、变量或属性的类型
+        作为数组、字典或其他容器中的元素类型
+         
+         协议是一种类型，因此协议类型的名称应与其他类型（例如 Int，Double，String）的写法相同，使用大写字母开头的驼峰式写法，例如（FullyNamed 和 RandomNumberGenerator）。
+         
+        */
+        
+        
+        //委托模式
+        //委托是一种设计模式，它允许类或结构体将一些需要它们负责的功能委托给其他类型的实例。委托模式的实现很简单：定义协议来封装那些需要被委托的功能，这样就能确保遵循协议的类型能提供这些功能。委托模式可以用来响应特定的动作，或者接收外部数据源提供的数据，而无需关心外部数据源的类型。
+        
+
+        let tracker = DiceGameTracker.init()
+        let game = SnakesAndLadders.init()
+        game.delegate = tracker
+        game.play()
+        
+        
+        //
         
         
         
